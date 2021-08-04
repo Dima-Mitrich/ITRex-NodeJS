08041a6960d6ed9aad8ebcb82966c20a7d9d0631
Dima-Mitrich <dima.mitrichenko@gmail.com>
2021-08-04 16:12:19 +0300

    lesson-2_task-4 : added solution for odd and even magic square

--- lesson-2/task-4/index.js
+++ lesson-2/task-4/index.js
@@ -1,108 +1,96 @@
 "use strict";
 
-function magicSquare(n) {
-    if (n === 1) return [1];
+function oddMagicSquare(n) {
 
-    if (n === 2) return 'impossible';
-
-    let startArrWithNumbers = [[]];
     let magicSquareArr = new Array(n);
 
     for (let i = 0; i < n; i++) {
         magicSquareArr[i] = new Array(n);
     }
 
-    (function fillArrWithNumbers() {
-        let j = 0;
-
-        for (let i = 1; i <= n * n; i++) {
-            startArrWithNumbers[j].push(i);
-
-            if (i % n === 0 && j < n - 1) {
-                startArrWithNumbers.push([]);
-                j++;
-            }
-        }
-    }());
-
     let middle = Math.floor(n / 2);
+    let i = 0;
+    let j = middle;
 
-    /*
-    (function fillBothDiagonal() {
-        for (let i = 0, j = n - 1; i < n, j >= 0; i++, j--) {
-            magicSquareArr[i][i] = startArrWithNumbers[i][middle];
-            magicSquareArr[j][i] = startArrWithNumbers[middle][i];
+    for (let a = 1; a <= n * n; a++) {
+
+        magicSquareArr[i][j] = a;
+
+        let test_i = i
+        test_i -= 1;
+
+        let test_j = j;
+        test_j += 1;
+
+        if (test_i < 0) {
+            test_i = n - 1;
         }
-    }());*/
 
-    (function () {
-        let x = 0;
-        let y = middle;
-
-        let z = 1;
-        let q = middle;
-
-        for (let i = 0; i < n; i++) {
-
-            if (i % 2 === 0) {
-                let a = x;
-                let b = y;
-
-                for (let j = 0; j < n; j += 2) {
-                    magicSquareArr[i][j] = startArrWithNumbers[a][b];
-                    b++;
-                    a++;
-                }
-
-                x += 1;
-                y -= 1;
-            }
-            else if (i % 2 !== 0) {
-                let a = z;
-                let b = q;
-
-                for (let j = 1; j < n; j += 2) {
-                    magicSquareArr[i][j] = startArrWithNumbers[a][b];
-                    a++;
-                    b++;
-                }
-
-                z += 1;
-                q -= 1;
-            }
+        if (test_j > n - 1) {
+            test_j = 0;
         }
-    }());
 
-    /*
-    (function () {
-        for (let i = 0; i < n; i++) {
-
-            if (i % 2 === 0) {
-                let x = middle + 1;
-                let y = 0;
-
-                for (let j = 1; j < n; j += 2) {
-                    magicSquareArr[i][j] = startArrWithNumbers[x][y];
-                    x++;
-                    y++;
-                    if (x > n - 1) {
-                        x = 0;
-                    }
-                    if (y > n - 1) {
-                        y = 0
-                    }
-                }
-            }
-            if (i % 2 !== 0) {
-                for (let j = 1; j < n; j += 2) {
-
-                }
-            }
-
+        if (magicSquareArr[test_i][test_j]) {
+            i = i + 1;
+            j = j;
         }
-    }());*/
+        else if (!magicSquareArr[test_i][test_j]) {
+            i = test_i;
+            j = test_j
+        }
+    }
 
-    console.log(magicSquareArr);
+    return magicSquareArr;
 }
 
-magicSquare(7);
+
+function evenMagicSquare(n) {
+
+    let magicSquareArr = new Array(n);
+
+    for (let i = 0; i < n; i++) {
+        magicSquareArr[i] = new Array(n);
+    }
+    let a = 1;
+    let b = n * n;
+
+    for (let i = 0; i < n; i += 1) {
+
+        for (let j = 0; j < n; j += 1) {
+
+            if (i % 4 === j % 4 || (i + j) % 4 === 3) {
+                magicSquareArr[i][j] = b;
+            }
+            else {
+                magicSquareArr[i][j] = a;
+            }
+
+            b -= 1;
+            a += 1;
+        }
+    }
+
+    return magicSquareArr;
+}
+
+
+function buildMagicSquare(n) {
+
+    switch (n) {
+        case 1:
+            return [1];
+        case 2:
+            return 'Impossible';
+        default:
+            return (
+                n % 2 === 0 ? evenMagicSquare(n) : oddMagicSquare(n)
+            );
+    }
+    
+}
+
+console.log(buildMagicSquare(3));
+console.log(buildMagicSquare(4));
+console.log(buildMagicSquare(5));
+console.log(buildMagicSquare(6));
+console.log(buildMagicSquare(7));
