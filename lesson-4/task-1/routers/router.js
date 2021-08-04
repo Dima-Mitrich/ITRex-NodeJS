5d0daa07d81b0fa59fda52426cb42b8c9014f98a
Dima-Mitrich <dima.mitrichenko@gmail.com>
2021-08-04 11:54:50 +0300

    lesson-4_task-1 : added express in project, deleted unnecessary packages, fixed bugs

---
+++ lesson-4/task-1/routers/router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express();
+const STATUSES = require('./statuses.js')
+
+router.get('/', (req, res) => {
+    res.status(STATUSES.OK).sendFile('../index.html');
+})
+
+module.exports = router;
---
+++ lesson-4/task-1/routers/statuses.js
@@ -0,0 +1,10 @@
+const STATUSES = {
+    OK: 200,
+    Created: 201,
+    BadRequest: 400,
+    NotFound: 404,
+    ServerError: 500,
+    Unavaible: 503,
+};
+
+module.exports = STATUSES;
